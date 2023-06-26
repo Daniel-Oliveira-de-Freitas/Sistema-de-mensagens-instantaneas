d4a75fb5cdce70ac413f938be62948c77dde0356
ThaisdosSantos <thaisds2.aluno@unipampa.edu.br>
2023-06-26 17:59:15 -0300

    adding comments

--- QUIC-Message-GO/client/client.go
+++ QUIC-Message-GO/client/client.go
@@ -1,53 +1,63 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/json"
-	"encoding/pem"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"path"
-	"time"
+	"bufio"         // Ler a entrada do usuário.
+	"bytes"         // Tabalhar com buffers de bytes.
+	"crypto/tls"    //Configuração de TLS.
+	"crypto/x509"   // Trabalhar com certificados X.509
+	"encoding/json" // Utilizado para JSON marshaling e unmarshaling.
+	"encoding/pem"  // Decodificar dados de certificados codificados por PEM.
+	"fmt"           // imprimir mensagens
+	"io"            // Operações de entrada/saída
+	"log"           // Utilizado para registrar mensagens
+	"net/http"      // Solicitações HTTP e manipular respostas
+	"os"            // Acessar o ambiente do sistema operacional
+	"path"          // caminhos de arquivo
+	"time"          // Medir e manipular o tempo
 
-	"github.com/lucas-clemente/quic-go/http3"
+	"github.com/lucas-clemente/quic-go/http3" // implementação de ida e volta HTTP/3.
 )
 
+// Obtém o diretório de trabalho atual
 func main() {
 	currentPath, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
+	// Cria um novo RoundTripper para comunicação HTTP/3
 	roundTripper := &http3.RoundTripper{
 		TLSClientConfig: &tls.Config{
+			// Definir os certificados CA raiz para verificação TLS
 			RootCAs: getRootCA(currentPath),
 		},
 	}
+	// Fecha o RoundTripper ao final da função principal
 	defer roundTripper.Close()
 
+	// Cria um cliente HTTP com o RoundTripper customizado
 	client := &http.Client{
 		Transport: roundTripper,
 	}
 
-	addr := "https://localhost:8080/"
-	addr2 := "https://localhost:8080/mensagem"
+	// Definição das URLs de destino
+	addr := "https://localhost:8080/"          // URL para solicitação GET
+	addr2 := "https://localhost:8080/mensagem" // URL para solicitação POST
+
+	// Envia uma requisição GET para o servidor
 	rsp, err := client.Get(addr)
 	if err != nil {
 		panic(err)
 	}
 	defer rsp.Body.Close()
 
+	// Lê o corpo da resposta em um buffer
 	body := &bytes.Buffer{}
 	_, err = io.Copy(body, rsp.Body)
 	if err != nil {
 		panic(err)
 	}
 
+	// Imprime o comprimento e o conteúdo do corpo da resposta
 	log.Printf("Body length: %d bytes\n", body.Len())
 	log.Printf("Response body: %s\n", body.Bytes())
 
@@ -63,12 +73,14 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Cria uma mensagem JSON com o texto digitado
 	message := map[string]string{"texto": inputText}
 	jsonMessage, err := json.Marshal(message)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Envia uma requisição POST com a mensagem JSON
 	start := time.Now()
 	resp, err := client.Post(addr2, "application/json", bytes.NewBuffer(jsonMessage))
 	if err != nil {
@@ -76,10 +88,12 @@ func main() {
 	}
 	defer resp.Body.Close()
 
+	// Imprime o status da resposta e o tempo de ida e volta
 	log.Printf("Response status: %s", resp.Status)
 	log.Printf("RTT: %v\n", time.Since(start).Nanoseconds())
 }
 
+// Função para carregar os certificados de CA raiz de um arquivo PEM
 func getRootCA(certPath string) *x509.CertPool {
 	caCertPath := path.Join(certPath, "ca.pem")
 	caCertRaw, err := os.ReadFile(caCertPath)
@@ -87,18 +101,23 @@ func getRootCA(certPath string) *x509.CertPool {
 		panic(err)
 	}
 
+	// Decodifica o certificado codificado por PEM
 	p, _ := pem.Decode(caCertRaw)
 	if p.Type != "CERTIFICATE" {
 		panic("expected a certificate")
 	}
 
+	// ParseCertificate analisa o certificado do bloco PEM decodificado e retorna um objeto de certificado
 	caCert, err := x509.ParseCertificate(p.Bytes)
 	if err != nil {
-		panic(err)
+		panic(err) // erro ao analisar o certificado, entre em pânico e interrompa a execução
 	}
 
+	// Cria um novo pool de certificados para manter os certificados de CA raiz
 	certPool := x509.NewCertPool()
+	// AddCert adiciona um certificado ao pool
 	certPool.AddCert(caCert)
 
+	// Retorna o pool de certificados contendo o certificado CA raiz
 	return certPool
 }
--- QUIC-Message-GO/server/main.go
+++ QUIC-Message-GO/server/main.go
@@ -1,38 +1,50 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/lucas-clemente/quic-go/http3"
 	"log"
 	"net/http"
 	"os"
 	"path"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lucas-clemente/quic-go/http3"
 )
 
+// Definicação da função main
 func main() {
+	// Obtém o diretório de trabalho atual
 	currentPath, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Cria um novo roteador Gin
 	router := gin.New()
 
+	// Define uma rota para a raiz ("/") que retorna um JSON com a mensagem "Oi".
 	router.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"Oi": "OK"})
 	})
 
+	// Define uma rota para "/mensagem" que recebe uma requisição POST contendo um JSON
+	// com um campo "texto". A função associdada a essa rota lê o JSON e registra a
+	// mensagem recebida.
 	router.POST("/mensagem", func(c *gin.Context) {
+		// define um campo Texto do tipo string serializar ou desserializar a estrutura para JSON
 		var mensagem struct {
 			Texto string `json:"texto"`
 		}
+		// c é um objeto do contexto do GIN, ShouldBindJSON à variável err. Se ocorrer um erro durante o bind, um JSON de resposta com um status 400
 		if err := c.ShouldBindJSON(&mensagem); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+		// O valor do campo Texto da estrutura mensagem é impresso no log.
 		log.Println("Mensagem recebida do cliente:", mensagem.Texto)
 		c.JSON(http.StatusOK, gin.H{"status": "Mensagem recebida"})
 	})
- 
+
+	// Cria um novo servidor HTTP/3 usando a estrutura http3.Server. Define o endereço do servidor como "127.0.0.1:8080" e o roteador Gin como o manipulador de solicitações.
 	server := http3.Server{
 		Addr:    "127.0.0.1:8080",
 		Handler: router,
@@ -47,8 +59,11 @@ func main() {
 
 	// select {}
 
+	// Inicia o servidor HTTP/3 para escutar na porta especificada (8080) e usar o certificado e chave fornecidos.
+	// o caminho do certificado TLS/SSL e o caminho da chave privada correspondente ao certificado
 	err = server.ListenAndServeTLS(path.Join(currentPath, "cert.pem"), path.Join(currentPath, "private.key"))
 	if err != nil {
+		// Registra qualquer erro ocorrido durante a execução do servidor.
 		log.Printf("Server error: %v", err)
 	}
 }
