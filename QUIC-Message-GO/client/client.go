f36245cd5c073d7336b1c312105e89de8612ce5c
Thaís Dos Santos <96456496+ThaisdosSantos@users.noreply.github.com>
2023-06-26 19:10:32 -0300

    Update client.go
    retry loop in client class

--- QUIC-Message-GO/client/client.go
+++ QUIC-Message-GO/client/client.go
@@ -20,77 +20,86 @@ import (
 
 // Obtém o diretório de trabalho atual
 func main() {
-	currentPath, err := os.Getwd()
-	if err != nil {
-		panic(err)
+	for {
+		currentPath, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		// Cria um novo RoundTripper para comunicação HTTP/3
+		roundTripper := &http3.RoundTripper{
+			TLSClientConfig: &tls.Config{
+				// Definir os certificados CA raiz para verificação TLS
+				RootCAs: getRootCA(currentPath),
+			},
+		}
+		// Fecha o RoundTripper ao final da função principal
+		defer roundTripper.Close()
+
+		// Cria um cliente HTTP com o RoundTripper customizado
+		client := &http.Client{
+			Transport: roundTripper,
+		}
+
+		// Definição das URLs de destino
+		addr := "https://localhost:8080/"          // URL para solicitação GET
+		addr2 := "https://localhost:8080/mensagem" // URL para solicitação POST
+
+		// Envia uma requisição GET para o servidor
+		rsp, err := client.Get(addr)
+		if err != nil {
+			panic(err)
+		}
+		defer rsp.Body.Close()
+
+		// Lê o corpo da resposta em um buffer
+		body := &bytes.Buffer{}
+		_, err = io.Copy(body, rsp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		// Imprime o comprimento e o conteúdo do corpo da resposta
+		log.Printf("Body length: %d bytes\n", body.Len())
+		log.Printf("Response body: %s\n", body.Bytes())
+
+		// Solicita ao usuário que digite um texto de mensagem
+		var inputText string
+		fmt.Print("Enter the message text: ")
+		// Cria um novo leitor para ler a entrada da entrada padrão (teclado)
+		reader := bufio.NewReader(os.Stdin)
+		// Lê a entrada até que um caractere de nova linha ('\n') seja encontrado
+		// A entrada é armazenada na variável 'inputText', e qualquer erro que ocorra é atribuído a 'err'
+		inputText, err = reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Remove o caractere de nova linha ('\n')
+		inputText = inputText[:len(inputText)-1]
+
+		if inputText == "sair" || inputText == "Sair" {
+			break // Encerra o laço de repetição se o usuário digitar "sair"
+		}
+
+		// Cria uma mensagem JSON com o texto digitado
+		message := map[string]string{"texto": inputText}
+		jsonMessage, err := json.Marshal(message)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Envia uma requisição POST com a mensagem JSON
+		start := time.Now()
+		resp, err := client.Post(addr2, "application/json", bytes.NewBuffer(jsonMessage))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		// Imprime o status da resposta e o tempo de ida e volta
+		log.Printf("Response status: %s", resp.Status)
+		log.Printf("RTT: %v\n", time.Since(start).Nanoseconds())
 	}
-	// Cria um novo RoundTripper para comunicação HTTP/3
-	roundTripper := &http3.RoundTripper{
-		TLSClientConfig: &tls.Config{
-			// Definir os certificados CA raiz para verificação TLS
-			RootCAs: getRootCA(currentPath),
-		},
-	}
-	// Fecha o RoundTripper ao final da função principal
-	defer roundTripper.Close()
-
-	// Cria um cliente HTTP com o RoundTripper customizado
-	client := &http.Client{
-		Transport: roundTripper,
-	}
-
-	// Definição das URLs de destino
-	addr := "https://localhost:8080/"          // URL para solicitação GET
-	addr2 := "https://localhost:8080/mensagem" // URL para solicitação POST
-
-	// Envia uma requisição GET para o servidor
-	rsp, err := client.Get(addr)
-	if err != nil {
-		panic(err)
-	}
-	defer rsp.Body.Close()
-
-	// Lê o corpo da resposta em um buffer
-	body := &bytes.Buffer{}
-	_, err = io.Copy(body, rsp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	// Imprime o comprimento e o conteúdo do corpo da resposta
-	log.Printf("Body length: %d bytes\n", body.Len())
-	log.Printf("Response body: %s\n", body.Bytes())
-
-	// Solicita ao usuário que digite um texto de mensagem
-	var inputText string
-	fmt.Print("Enter the message text: ")
-	// Cria um novo leitor para ler a entrada da entrada padrão (teclado)
-	reader := bufio.NewReader(os.Stdin)
-	// Lê a entrada até que um caractere de nova linha ('\n') seja encontrado
-	// A entrada é armazenada na variável 'inputText', e qualquer erro que ocorra é atribuído a 'err'
-	inputText, err = reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Cria uma mensagem JSON com o texto digitado
-	message := map[string]string{"texto": inputText}
-	jsonMessage, err := json.Marshal(message)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Envia uma requisição POST com a mensagem JSON
-	start := time.Now()
-	resp, err := client.Post(addr2, "application/json", bytes.NewBuffer(jsonMessage))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	// Imprime o status da resposta e o tempo de ida e volta
-	log.Printf("Response status: %s", resp.Status)
-	log.Printf("RTT: %v\n", time.Since(start).Nanoseconds())
 }
 
 // Função para carregar os certificados de CA raiz de um arquivo PEM
